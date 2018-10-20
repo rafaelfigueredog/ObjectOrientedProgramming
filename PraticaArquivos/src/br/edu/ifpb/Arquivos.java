cc922e1b4a98ed9f753bfdabffa73a09496f7d6f
Rafael Guimarães <rafaelfigueredog@gmail.com>
2018-10-20 12:22:30 -0300

    pratica arquivos

--- PraticaArquivos/src/br/edu/ifpb/Arquivos.java
+++ PraticaArquivos/src/br/edu/ifpb/Arquivos.java
@@ -1,14 +1,33 @@
 package br.edu.ifpb;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
+import java.security.cert.TrustAnchor;
+import java.util.Scanner;
 
 public class Arquivos {
 
-
     public static void leitor (String path) throws IOException {
-        BufferedReader Buffreader
+        FileReader fr = new FileReader(path);
+        BufferedReader buffreader = new BufferedReader(fr);
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+            }else
+                break;
+            linha = buffreader.readLine();
+        }
+        buffreader.close();
     }
 
+    public static void escritor (String pach) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(pach));
+        String linha = "";
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Linha: ");
+        linha = in.next();
+        bw.append(linha +"\n");
+        bw.close();
+    }
 
 }
