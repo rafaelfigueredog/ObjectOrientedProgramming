84ff9e8e3dc843351011be40179ba70e26a58a32
Rafael Guimarães <rafaelfigueredog@gmail.com>
2018-11-13 03:27:03 -0300

    Sequencia Recursiva add

---
+++ CaleumJava/src/br/edu/ifpb/SequenciaRecursiva.java
@@ -0,0 +1,26 @@
+package br.edu.ifpb;
+
+public class SequenciaRecursiva {
+
+    public static void SequenciaRecursiva(int x) {
+
+        if (x == 1) {
+            System.out.println(x);
+            return;
+        }
+        System.out.print(x  +  ", ");
+        if ( x % 2 == 0)
+            SequenciaRecursiva( x/2  );
+        else
+            SequenciaRecursiva((3 * x) + 1);
+
+
+    }
+
+    public static void main(String[] args) {
+
+        SequenciaRecursiva(13);
+
+    }
+
+}
